cce5e5bc4b302bc1fd6f9826645d8130231b60dd
SamraAlam171 <sp24-bse-137@cuilahore.edu.pk>
2024-09-19 15:59:58 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+public static void main(String args[]){
+
+//instantiation
+Artist artist = new Artist("Thomos");
+Artwork a1 = new Artwork("Art piece", 1999);
+Artwork a2 = new Artwork("Art piece2", 1899);
+
+artist.displayInfo();
+a1.displayInfo();
+
+
+}
+}
+
+//Samra Alam
+//sp24-bse-137
